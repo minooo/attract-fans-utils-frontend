f3d1b77cd81bc271eaff19d94cbb35ec5cd50152
duzhihao <782513616@qq.com>
2018-07-31 17:20:20 +0800

    xiufu

--- src/1-config/app_config.js
+++ src/1-config/app_config.js
@@ -6,7 +6,7 @@ export default {
   id: 0, // 应用ID
   version: "",
   publicPath: "/", // 静态资源存放路径, 开发模式不需要设置
-  api: "http://mp.dev.duduapp.net/h5/zKB4Db0a2PVqyV8YPve3", // 重点1 . 需要向后台索要api地址
+  api: "http://mp.dev.duduapp.net", // 重点1 . 需要向后台索要api地址
   wx: {
     apiList: [],
     share: {
